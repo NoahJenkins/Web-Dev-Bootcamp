80667f699f94522159d83d37962495fbd1f584d7
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2024-12-31 15:29:34 -0600

    basic sum calc

--- 006 JavaScript/basics.js
+++ 006 JavaScript/basics.js
@@ -1,3 +1,7 @@
+// var first_number = prompt("Enter the first number");
+// var second_number = prompt("Enter the second number");
+// var sum = Number(first_number) + Number(second_number);
+
 var first_number = prompt("Enter the first number");
 var second_number = prompt("Enter the second number");
-var sum = Number(first_number) + Number(second_number);
+alert(Number(first_number) + Number(second_number));
