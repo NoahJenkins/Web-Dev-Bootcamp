a724e62c7c676edf6cd035fc3b25b9905c98340a
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-02 07:06:30 -0600

    keyless car

---
+++ 006 JavaScript Basics/keyless_car.js
@@ -0,0 +1,8 @@
+var age = prompt("What is your age?");
+
+if (age < 18) {
+  alert("Sorry, you are too young to drive this car. Powering off");
+}
+else if (age >= 18) {
+  alert("Powering On. Enjoy the ride!");
+}
