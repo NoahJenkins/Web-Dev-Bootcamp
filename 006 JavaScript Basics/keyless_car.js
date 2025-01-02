b9201b9e34b23df9bcd826087c9e32d4717f6d2f
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-02 15:18:49 -0600

    keyless car upgrade

--- 006 JavaScript Basics/keyless_car.js
+++ 006 JavaScript Basics/keyless_car.js
@@ -3,6 +3,26 @@ var age = prompt("What is your age?");
 if (age < 18) {
   alert("Sorry, you are too young to drive this car. Powering off");
 }
-else if (age >= 18) {
+else if (age > 18) {
   alert("Powering On. Enjoy the ride!");
 }
+else if (age === 18) {
+  alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+
+function checkDriverAge() {
+  var age = prompt("What is your age?");
+
+  if (age < 18) {
+    ("Sorry, you are too young to drive this car. Powering off");
+  }
+  else if (age > 18) {
+    alert("Powering On. Enjoy the ride!");
+  }
+  else if (age === 18) {
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+
+}
+
+checkDriverAge();
