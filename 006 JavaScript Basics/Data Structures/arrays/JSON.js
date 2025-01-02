700c34e1e578f3cd0c5daad7813857bcd2dcfd95
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-02 16:12:03 -0600

    JSON

---
+++ 006 JavaScript Basics/Data Structures/arrays/JSON.js
@@ -0,0 +1,15 @@
+var user = {
+    name: "John",
+    age: 34,
+    hobby: "Soccer",
+    isMarried: false
+};
+
+console.log(user.age);
+console.log(user.hobby);
+
+user.favoriteFood = "spinach";
+console.log(user.favoriteFood);
+
+user.isMarried = true;
+// output: isMarried: true instead of isMarried: false
