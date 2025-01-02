f3e5a5c4932e297fa38df766e75da26f273c63c9
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-02 15:02:49 -0600

    functions

---
+++ 006 JavaScript Basics/functions/script.js
@@ -0,0 +1,25 @@
+function sayHello() {
+    console.log('Hello!');
+}
+
+sayHello();
+
+// Anonymous function is a function without a name, in this case it is assigned to a variable
+var sayBye = function() {
+    console.log('Bye!');
+}
+
+sayBye();
+
+function sing(song) {
+    console.log(song);
+}
+
+sing('Laaa deee daaa');
+sing("Good morning!");
+sing("backstreet's back alright");
+
+function multiply(a,b) {
+    return a * b;
+    
+}
