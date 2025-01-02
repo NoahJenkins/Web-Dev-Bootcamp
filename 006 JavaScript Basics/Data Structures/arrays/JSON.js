54454d7b8ae5476dfd23b4711343939d5dc6eddd
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-02 16:55:03 -0600

    JSON

--- 006 JavaScript Basics/Data Structures/arrays/JSON.js
+++ 006 JavaScript Basics/Data Structures/arrays/JSON.js
@@ -2,7 +2,11 @@ var user = {
     name: "John",
     age: 34,
     hobby: "Soccer",
-    isMarried: false
+    isMarried: false,
+    spells: ["abrakadabra", "shazam", "boo"],
+    shout; function() {
+        console.log("AHHHHHHH!");
+    }
 };
 
 console.log(user.age);
@@ -13,3 +17,27 @@ console.log(user.favoriteFood);
 
 user.isMarried = true;
 // output: isMarried: true instead of isMarried: false
+
+user.spells[1];
+// output: shazam
+
+var list = [
+    {
+        username: "andy",
+        password: "secret",
+    },
+    {
+        username: "jess",
+        password: "123",
+    }
+];
+
+list[0].password;
+// output: secret
+
+user.shout();
+// output: AHHHHHHH!
+// shout is a method of the user object
+
+console.log()
+//console is a global object in JavaScript that is used to log information to the console.
