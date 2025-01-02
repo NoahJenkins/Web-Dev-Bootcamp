9679d9c855242162f2855c7d62ef8e3d0bb28e55
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-02 07:04:33 -0600

    basic js

---
+++ 006 JavaScript Basics/basics.js
@@ -0,0 +1,41 @@
+// var first_number = prompt("Enter the first number");
+// var second_number = prompt("Enter the second number");
+// var sum = Number(first_number) + Number(second_number);
+
+var first_number = prompt("Enter the first number");
+var second_number = prompt("Enter the second number");
+
+// explain the alert function
+alert(Number(first_number) + Number(second_number));
+
+// if (name === "John") {
+//   alert("Hello John");
+// } else {
+//   alert("Hello Stranger");
+// }
+
+// if (name === "John") {
+//   alert("Hello John");
+// } else if (name === "Jane") {
+//   alert("Hello Jane");
+// } else {
+//   alert("Hello Stranger");
+// }
+
+if (name === "Billy") or (name === "Bob")
+  alert("Hello Billy or Bob");
+
+// || is the OR operator in the if statement
+if (name === "Noah" || name === "Liam")
+  alert("Hello Noah or Liam");
+
+if (firstName === "John") and (lastName === "Doe")
+  alert("Hello John Doe");
+
+// && is the AND operator in the if statement
+if (firstName === "John" && lastName === "Doe")
+  alert("Hello John Doe");
+
+// the ! operator is the NOT operator
+if (!name === "John")
+  alert("Hello not John");
--- 006 JavaScript/basics.js
+++
@@ -1,7 +0,0 @@
-// var first_number = prompt("Enter the first number");
-// var second_number = prompt("Enter the second number");
-// var sum = Number(first_number) + Number(second_number);
-
-var first_number = prompt("Enter the first number");
-var second_number = prompt("Enter the second number");
-alert(Number(first_number) + Number(second_number));
