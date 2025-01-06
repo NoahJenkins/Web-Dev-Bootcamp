f2b5088f97e8ed696e4bc62746aa3001d7aacb9e
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-06 16:49:56 -0600

    testing button function 2

--- 007 DOM (Document Object Model)/DOM/script.js
+++ 007 DOM (Document Object Model)/DOM/script.js
@@ -7,8 +7,12 @@
 
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
     console.log("click is working");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
 }
 );
