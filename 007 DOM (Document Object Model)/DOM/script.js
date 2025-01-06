2f28359017dae1fa0d59132808e34197df15692e
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-06 16:37:20 -0600

    event listener

---
+++ 007 DOM (Document Object Model)/DOM/script.js
@@ -0,0 +1,14 @@
+// var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener('click', function() {
+//     console.log("Button clicked");
+//     alert("You clicked me!" );
+// });
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+
+button.addEventListener("click", function() {
+    console.log("click is working");
+}
+);
