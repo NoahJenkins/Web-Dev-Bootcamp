e7da0283a8e0a9b29701b84a581f9a9f09cbcb3f
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-01-30 06:39:27 -0600

    es2019 notes

--- 008 Advance JS/011 ES2019/script.js
+++ 008 Advance JS/011 ES2019/script.js
@@ -2,7 +2,27 @@ const array = [1,2,3,4,5]
 console.log(array.flat())
 
 const array2 = [1,[2,3],[4,5]]
-console.log(array2.flat())
+console.log(array2.flat())//outputs [ 1, 2, 3, 4, 5 ]
 
 const array3 = [1,[2,3,[4,5]]]
-console.log(array3.flat())
+console.log(array3.flat()) // outputs [ 1, 2, 3, [ 4, 5 ] ]
+console.log(array3.flat(2)) //outputs [ 1, 2, 3, 4, 5 ]
+console.log(array3.flat(50)) //outputs [ 1, 2, 3, 4, 5 ]
+
+const entries = ['bob', 'sally',,,,,,,,,,,"cindy"]
+console.log(entries.flat()) //returns [ 'bob', 'sally', 'cindy' ]
+
+const userEmail = '         eddytheeagle@gmail.com'
+const userEmail2 = 'jonnydangerous@gmail.com             '
+console.log(userEmail.trimStart()) // eddytheeagle@gmail.com
+console.log(userEmail2.trimEnd()) // jonnydangerous@gmail.com
+
+const UserProfiles = [['commanderTom', 23],["derekZlander", 40], ["hanzel", 21]]
+console.log(Object.fromEntries(UserProfiles)) // returns the array as an object
+// console.log(Object.entries(obj)) //returns an array from the object
+
+// try {
+//     bob +"hi"
+// } catch {
+//     console.log("you done goofed")
+// }
