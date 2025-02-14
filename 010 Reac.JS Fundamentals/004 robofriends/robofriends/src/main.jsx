cdc7364e1b5aee3ccdbf5b9e029b6ec6dddb7152
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-02-14 09:45:06 -0600

    react component

--- 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/main.jsx
+++ 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Hello from './Hello.jsx'
+import "tachyons/css/tachyons.min.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Hello greeting={"Hello" + "Ninja"} />
   </StrictMode>,
 )
