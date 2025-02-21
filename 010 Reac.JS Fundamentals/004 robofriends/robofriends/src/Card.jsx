bcac6ab6a39ab998e766cc4bd56aa20f3a470009
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-02-21 17:28:23 -0600

    building a react app

--- 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/Card.jsx
+++ 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/Card.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const Card = {
-    
+
+const Card = (props) => {
+    const {name, email, id} = props;
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${props.id}?200x200`} alt='Card image' />
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
+            </div>
+        </div>
+    );
 };
+
+export default Card;
--- 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/main.jsx
+++ 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Card from './Card.jsx'
 import "tachyons/css/tachyons.min.css";
+import { robots } from "./robots.js"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Hello greeting={"Hello" + "Ninja"} />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
   </StrictMode>,
 )
