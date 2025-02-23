4b7506b34559d42f1907cc1dfd2469c05f39c7d3
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-02-23 11:25:42 -0600

    scrollable component

--- 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/App.jsx
+++ 010 Reac.JS Fundamentals/004 robofriends/robofriends/src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox.jsx";
-import { robots } from "./robots";
+import Scroll from "./scroll.jsx";
 
 class App extends React.Component {
     constructor() { 
         // The constructor is used for initializing the component's state
         super(); // Calls the constructor of the parent class (React.Component)
         this.state = {
-            robots: robots, // Initial state for the list of robots
+            robots: [], // Initial state for the list of robots
             searchfield: "", // Initial state for the search field (empty string)
         };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response=> response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
     // Event handler for updating searchfield state when user types in the search box
@@ -25,14 +31,20 @@ class App extends React.Component {
         );
         console.log(filteredRobots)
 
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>;
+        } else {
+
         return (
             <div className="tc">
                 <h1>RoboFriends</h1> {/* Page title */}
                 <SearchBox searchChange={this.OnSearchChange} /> {/* Search input */}
+                <Scroll>
                 <CardList robots={filteredRobots} /> {/* Displays filtered robots */}
+                </Scroll>
             </div>
         );
-    }
+    }}
 }
 
 export default App;
