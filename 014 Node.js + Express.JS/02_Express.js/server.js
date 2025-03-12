c65b528e472f7c4f9eead3a8e73e7e0dc63d813d
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-03-12 06:53:58 -0500

    adding gitignore

--- 014 Node.js + Express.JS/02_Express.js/server.js
+++ 014 Node.js + Express.JS/02_Express.js/server.js
@@ -2,16 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-    });
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+//     });
 
-    app.get('/profile', (req, res) => {
-        const users = [
-            {name: 'John', age: 25},
-            {name: 'Jane', age: 22}
-        ];
-        res.send(users);
-    });
+//     app.get('/profile', (req, res) => {
+//         const users = [
+//             {name: 'John', age: 25},
+//             {name: 'Jane', age: 22}
+//         ];
+//         res.send(users);
+//     });
+
+app.use((req, res, next) => {
+    console.log('<h1>Hello World</h1>');
+    next();
+    res.send('<h1>Hello World</h1>');
+})
 
 app.listen(3000);
