8c744f1cd9a455ed72d0c780379a991856a70a0b
Noah Jenkins <41129202+NoahJenkins@users.noreply.github.com>
2025-03-12 20:13:54 -0500

    read, append, and write text

---
+++ 014 Node.js + Express.JS/04_Node File System/script.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+fs.readFile('./hello.txt', (err, data) => { //readFile is asynchronous, it will run in the background and will not block the code
+    if (err) {
+        console.log('errror');
+    }
+    else {
+        console.log(data.toString()); //we add toString() to convert the buffer to a string, otherwise it will print a buffer (raw data)
+    }
+});
+
+const file = fs.readFileSync('./hello.txt'); //readFileSync is synchronous, it will block the code until it finishes
+console.log("2",file.toString());
+
+// fs.appendFile('./hello.txt', ' This is so cool!', err => { //appendFile will add the text to the file
+//     if (err) {
+//         console.log(err);
+//     }
+// });
+
+fs.writeFile('bye.txt', 'Sad to see you go', err => { //writeFile will create a new file with the text)
+    if (err) {
+        console.log(err);
+    }});
